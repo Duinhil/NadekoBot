60d87fd6d03829cdb2ca9fb17045152d687e4c5a
Andrew Beards <abeards@hotmail.co.uk>
2020-07-28 22:07:42 +0100

    Adding DB migrations

---
+++ NadekoBot.Core/Migrations/20200728210322_add_column_defaultautoplay.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NadekoBot.Migrations
+{
+    public partial class add_column_defaultautoplay : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DefaultAutoPlay",
+                table: "GuildConfigs",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultAutoPlay",
+                table: "GuildConfigs");
+        }
+    }
+}
